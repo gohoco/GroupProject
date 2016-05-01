c35b83f2151019276be930cf351def817a5db2b2
gohoco <gohoco@yahoo.com.hk>
2016-05-01 22:01:24 +0800

    add function get parent and get child

--- PageRank.java
+++ PageRank.java
@@ -46,12 +46,12 @@ public class PageRank {
 		
 	}
 	
-	public HashStruc getParent(){
-		return parent;
+	public Vector<String> getParent(String UrlID) throws IOException{
+		return (Vector<String>) parent.getEntry(UrlID);
 	}
 	
-	public HashStruc getChild(){
-		return child;
+	public Vector<String> getChild(String UrlID) throws IOException{
+		return (Vector<String>) child.getEntry(UrlID);
 	}
 	
 	public double getScore(String UrlID) throws IOException{
